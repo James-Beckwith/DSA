4b31e6e568b09f96b1c0f846578712f31bcf8fd6
James-Beckwith <py06jab@gmail.com>
2018-11-04 17:32:31 +0000

    updated fkFilter function. Still incomplete

--- fkFilter.py
+++ fkFilter.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.image import AxesImage
+from matplotlib.path import Path
 
 '''Class to define, store and apply FK filter to a 2D numpy arrayself.'''
 class fkFilter:
@@ -7,6 +9,10 @@ class fkFilter:
     ''' utility to find the next integer power of two of the input '''
     def nextpow2(self, input):
         return int(2 ** np.ceil(np.log(input) / np.log(2.0)))
+
+    def extents(self, f):
+      delta = f[1] - f[0]
+      return [f[0] - delta/2, f[-1] + delta/2]
 
     def __init__(self, inputData, sampleRateTime, sampleRateSpace, rampLength = 10, rampFunc = None, fkDomainFilter = None):
         ''' Initialization.
@@ -54,11 +60,43 @@ class fkFilter:
     def pickWindow(self):
 
         # plot FK amplitude reponse for positive frequencies only
-        plt.figure
+        fig, ax = plt.subplots()
         plt.imshow(np.abs(self.inputDataFK[(len(self.freqTimePositive) - 1):,:]), aspect='auto', interpolation='none',
-           extent=extents(self.wavenumber) + extents(self.freqTimePositive))
+            extent=self.extents(self.wavenumber) + self.extents(self.freqTimePositive), picker=True)
+
+        picks=[]
+        # define what to do when a pick is made, mouse button 1-> store picks,
+        # mouse button 3->stop picking
+        def onpick(event):
+            if event.button==1:
+                picks.append((event.xdata, event.ydata))
+                if len(picks)>1:
+                    ax.plot([picks[-2][0], picks[-1][0]],[picks[-2][1], picks[-1][1]],"w-")
+                    fig.canvas.draw()
+                    fig.canvas.flush_events()
+            if event.button==3:
+                plt.close()
+
+        fig.canvas.mpl_connect('button_press_event', onpick)
 
         plt.show()
+
+        # convert picks to polygon
+        # define meshgrid of points
+        x, y = np.meshgrid(self.wavenumber, self.freqTimePositive,)
+        x, y = x.flatten(), y.flatten()
+        points = np.vstack((x,y)).T
+        p = Path(picks)
+        grid = p.contains_points(points)
+        FKpolygon = grid.reshape(len(self.freqTimePositive), len(self.wavenumber))
+        FKpolygon.astype(float)
+
+        plt.figure()
+        plt.imshow(FKpolygon, aspect='auto')
+        plt.show()
+
+        # apply ramp function to FKpolygon
+
 
     ''' apply pre-defined FK window function in FK domain and inverse FFT '''
     def applyFKFilter(self):
