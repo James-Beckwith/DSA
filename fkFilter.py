77a30139224d8e459ecda5aed036bb973997264d
James-Beckwith <py06jab@gmail.com>
2018-11-10 21:59:26 +0000

    added ramp function to window generation

--- fkFilter.py
+++ fkFilter.py
@@ -30,7 +30,7 @@ class fkFilter:
         if rampFunc is not None:
             self.rampFunc = rampFunc
         else:
-            self.rampFunc = np.arange(0, 1.0+1.0/(rampLength - 1), 1.0/(rampLength - 1))
+            self.rampFunc = np.arange(1, 0.0-1.0/(rampLength - 1), -1.0/(rampLength - 1))
 
         # store size of input data
         self.numberTime, self.numberSpace = np.shape(inputData)
@@ -50,10 +50,10 @@ class fkFilter:
         self.inputDataFK = self.inputDataFK
 
         # define frequency axes
-        tempAddition = 1.0 / (self.numberFrequency / 2.0 + 1.0)
+        tempAddition = 1.0 / (self.numberFrequency / 2.0)
         self.freqTimePositive = 1.0 / (2.0 * self.sampleRateTime) * np.arange(0.0, 1.0 + tempAddition, tempAddition)
-        tempAddition = 1.0 / (self.numberWavenumber)
-        self.wavenumber = 1.0 / (2.0 * self.sampleRateSpace) * np.arange(-1.0 - tempAddition, 1.0 + tempAddition, tempAddition)
+        tempAddition = 1.0 / ((self.numberWavenumber - 1.0)/ 2.0)
+        self.wavenumber = 1.0 / (2.0 * self.sampleRateSpace) * np.arange(-1.0, 1.0 + tempAddition, tempAddition)
 
 
     ''' function to pick a window based on the FK domain response of the input data '''
@@ -69,7 +69,7 @@ class fkFilter:
         # mouse button 3->stop picking
         def onpick(event):
             if event.button==1:
-                picks.append((event.xdata, event.ydata))
+                picks.append([event.xdata, event.ydata])
                 if len(picks)>1:
                     ax.plot([picks[-2][0], picks[-1][0]],[picks[-2][1], picks[-1][1]],"w-")
                     fig.canvas.draw()
@@ -81,29 +81,109 @@ class fkFilter:
 
         plt.show()
 
+        # convert pick data to sample locations
+        picksSampX = [(np.abs(picks[i][0] - self.wavenumber)).argmin() for i in range(len(picks))]
+        picksSampY = [(np.abs(picks[i][1] - self.freqTimePositive)).argmin() for i in range(len(picks))]
+        # combine data back together
+        picksSamp = np.vstack((picksSampX, picksSampY)).T.tolist()
+
         # convert picks to polygon
         # define meshgrid of points
-        x, y = np.meshgrid(self.wavenumber, self.freqTimePositive,)
+        x, y = np.meshgrid(np.arange(self.numberWavenumber), np.arange(len(self.freqTimePositive)))
         x, y = x.flatten(), y.flatten()
         points = np.vstack((x,y)).T
-        p = Path(picks)
+
+        p = Path(picksSamp)
         grid = p.contains_points(points)
-        FKpolygon = grid.reshape(len(self.freqTimePositive), len(self.wavenumber))
-        FKpolygon.astype(float)
+        self.FKpolygon = grid.reshape(len(self.freqTimePositive), len(self.wavenumber))
+        self.FKpolygon = self.FKpolygon.astype(float)
+
+        # apply ramp function if necessary
+        if (self.rampFunc is not None):
+            if len(self.rampFunc) > 1:
+                self.applyRamp(picksSamp)
+                self.FKpolygon += self.windowRamp
+                self.FKpolygon[self.FKpolygon>1] = 1
 
         plt.figure()
-        plt.imshow(FKpolygon, aspect='auto')
+        plt.imshow(self.FKpolygon, aspect='auto')
         plt.show()
 
-        # apply ramp function to FKpolygon
 
+    ''' apply ramp function to picked points '''
+    def applyRamp(self, picks):
+
+        # interpolate points onto grid samplingtion
+        # 'ramp' is ramp length
+        # interpolate between points (1D linear interpolation in 2D space)
+        marks=0
+        picks.append(picks[0])
+        rampLength = len(self.rampFunc)
+        line1=[]
+        #tmp=[picks;picks(1,:)];
+        for ii in range(len(picks) - 1):
+
+            # fit straight line between successive points
+            dx=picks[ii+1][0] - picks[ii][0]
+            dy=picks[ii+1][1] - picks[ii][1]
+            # try:
+            #     m=dy/dx
+            #     mInfFlag=0
+            #     c=picks[ii][1] - m*picks[ii][0]
+            # except:
+            #     mInfFlag = 1
+
+            if (dx!=0) or (dy!=0):
+                if abs(dx)>=abs(dy):
+                    m=dy/dx
+                    c=picks[ii][1] - m*picks[ii][0]
+                    tmp = np.arange(picks[ii][0], picks[ii+1][0] + dx/abs(dx), dx/abs(dx))
+                    #line1.append([tmp.tolist(), (m*tmp+c).tolist()])
+                    line1 += np.vstack([tmp.tolist(), (m*tmp+c).tolist()]).T.tolist()
+                    marks=marks+abs(dx)
+                else:
+                    #line1(marks:marks+abs(dy),2)=tmp(ii,2):dy/abs(dy):tmp(ii+1,2);
+                    tmp = np.arange(picks[ii][1], picks[ii+1][1] + dy/abs(dy), dy/abs(dy))
+                    if dx==0:
+                        #line1(marks:marks+abs(dy),1)=picks[ii][0]*len(tmp);
+                        #line1.append([[picks[ii][0]]*len(tmp), tmp.tolist()])
+                        line1 += np.vstack([[picks[ii][0]]*len(tmp), tmp.tolist()]).T.tolist()
+                    else:
+                        m=dy/dx
+                        c=picks[ii][1] - m*picks[ii][0]
+                        #line1(marks:marks+abs(dy),1)=(line1(marks:marks+abs(dy),2)-c)/m;
+                        #line1.append([((tmp - c)/m).tolist(), tmp.tolist()])
+                        line1 += np.vstack([((tmp - c)/m).tolist(), tmp.tolist()]).T.tolist()
+                    marks=marks+abs(dy)
+
+        # find lowest and highest poits of window in x and y
+        line1AsArray = np.asarray(line1)
+        minx=np.min(line1AsArray[:,0])
+        maxx=np.max(line1AsArray[:,0])
+        miny=np.min(line1AsArray[:,1])
+        maxy=np.max(line1AsArray[:,1])
+
+        self.windowRamp = np.zeros([len(self.freqTimePositive), self.numberWavenumber])
+
+        # speed up window generation by only investigating points with a ramps
+        # distance of the picked window boundary
+        for ii in range(len(self.freqTimePositive)):
+            if (ii + rampLength > miny) and (ii - rampLength < maxy):
+                for jj in range(self.numberWavenumber):
+
+                    if (jj + rampLength > minx) and (jj - rampLength < maxx):
+                        if self.FKpolygon[ii,jj]!=1:
+                            # define distance to nearest window edge
+                            tmp=((line1AsArray[:,0] - jj)**2 + (line1AsArray[:,1] - ii)**2)**0.5
+                            dist1=np.round(np.min(tmp))
+                            if dist1==0:
+                                self.windowRamp[ii,jj]=1
+                            elif dist1 <= rampLength:
+                                self.windowRamp[ii,jj]=self.rampFunc[int(dist1) - 1]
 
     ''' apply pre-defined FK window function in FK domain and inverse FFT '''
     def applyFKFilter(self):
-
-        # check if an FK domain window has been defined
-        if self.fkDomainFilter is None:
-            raise NameError('No FK domain window function defined')
+        print('poop')
 
     ''' create the impulse response of the FK domain filter in the time-space domain '''
     def impulseResponse(self):
